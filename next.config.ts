b271f2a9c69c93709cbd62915d282d0a73f90ef7
beom <gb3187@naver.com>
2024-10-22 18:05:23 +0900

    feat : 빌드오류 수정2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com"], // 허용할 도메인 추가
+  },
 };
 
 export default nextConfig;
